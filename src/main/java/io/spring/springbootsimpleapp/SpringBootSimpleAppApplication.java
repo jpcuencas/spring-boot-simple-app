bcd8c566f488d86dd911a8f71cb995bf8101eee0
jpcuencas <juan.pedro.cuenca.sevilla@gmail.com>
2021-04-08 19:18:36 +0200

    first commit

---
+++ src/main/java/io/spring/springbootsimpleapp/SpringBootSimpleAppApplication.java
@@ -0,0 +1,12 @@
+package io.spring.springbootsimpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSimpleAppApplication.class, args);
+    }
+}
